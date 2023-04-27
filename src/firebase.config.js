a4f7df69bc46f685ff562ee94f00ddb6db5f26ff
Ismail-Mirza <ismail.me.buet@gmail.com>
2023-04-27 22:57:56 +0600

    update server

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAKRFG40QQfNm3Y41wRojt5wD8QvIMoxSA",
+    authDomain: "creation-version.firebaseapp.com",
+    projectId: "creation-version",
+    storageBucket: "creation-version.appspot.com",
+    messagingSenderId: "456547930193",
+    appId: "1:456547930193:web:f32c5942151b5f80136870",
+};
+
+export default firebaseConfig;
